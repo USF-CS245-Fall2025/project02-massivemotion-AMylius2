28498749644d291b7638c862eebe7b97ac38ffeb
AMylius2 <abmylius@dons.usfca.edu>
2025-12-02 21:39:55 -0800

    Delete src/DoubleNode.java

--- src/DoubleNode.java
+++
@@ -1,8 +0,0 @@
-public class DoubleNode<T>{
-    T data;
-    DoubleNode<T> next;
-    DoubleNode<T> previous;
-    DoubleNode(T d) {//constructor which assigns data value
-        data = d;
-    }
-}
